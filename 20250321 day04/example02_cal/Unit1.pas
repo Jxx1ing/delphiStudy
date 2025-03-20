1b7967952f6852ee574569d38a79e0ba85f5f15a
Jxx1ing <jxx126@outlook.com>
2025-03-21 00:10:39 +0800

    20250321计算器V2

--- 20250321 day04/example02_cal/Unit1.pas
+++ 20250321 day04/example02_cal/Unit1.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls, Vcl.Imaging.jpeg;
 
 type
   TForm1 = class(TForm)
@@ -17,12 +18,19 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
+    Image1: TImage;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
     procedure Edit1Click(Sender: TObject);
     procedure Edit2Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -58,6 +66,24 @@ procedure TForm1.Button4Click(Sender: TObject);
 begin
   label2.Caption := '➗';
   edit4.Text := FloatToStr( StrToFloat(edit1.Text) / StrToFloat(edit2.Text) );
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  label2.Caption := 'AND';
+  edit4.text := IntToStr( StrToInt(edit1.Text) And StrToInt(edit2.Text) );
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+  label2.Caption := 'Or';
+  edit4.text := IntToStr( StrToInt(edit1.Text) Or StrToInt(edit2.Text) );
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+  label2.Caption := 'Not';
+  edit4.text := IntToStr( Not StrToInt(edit1.Text) );
 end;
 
 procedure TForm1.Edit1Click(Sender: TObject);
